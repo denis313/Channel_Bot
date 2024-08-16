ec542c4aee375f2b800bebc02e5edc7357913a59
Denis Evtushok <evtushokdenis2004@gmail.com>
2024-08-17 00:13:41 +0300

    update

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -16,5 +16,5 @@ db_manager = DatabaseManager(dsn=dsn)
 
 @router.message(CommandStart(), StateFilter(default_state))
 async def start_command(message: Message):
-    # await db_manager.create_tables()
+    await db_manager.create_tables()
     await message.answer(lexicon['start'], reply_markup=keyboard_buy)
